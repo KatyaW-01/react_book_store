f0ed737683e5178051098e7c624331e79c60ca55
Katya Weicht <katya.weicht@gmail.com>
2025-06-14 09:47:14 -0600

    Store value of input in state

--- src/pages/SearchBooks.jsx
+++ src/pages/SearchBooks.jsx
@@ -1,9 +1,19 @@
 import React from "react"
+import useBooks from "../hooks/useBooks"
+import { useState } from 'react';
 
 function SearchBooks() {
+  const {books} = useBooks()
+  const [searchTerm, setSearchTerm] = useState("")
+ 
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value) //stores value of input in state
+  }
+
   return (
     <>
-    <input type="text" placeholder="Search"></input>
+    <input type="text" placeholder="Search" value={searchTerm} onChange={handleChange}></input>
     </>
   )
 }
