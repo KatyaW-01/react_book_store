4905f5fff616ceb7f8c51d6bffa51a9125924937
Katya Weicht <katya.weicht@gmail.com>
2025-06-13 22:49:17 -0600

    Setup NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom"
+
+function NavBar() {
+  return (
+    <nav>
+      <Link>Home</Link>
+      <Link>Shop</Link>
+      <Link>Admin Portal</Link>
+    </nav>
+  )
+}
+
+export default NavBar
