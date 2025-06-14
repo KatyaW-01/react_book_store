5dc41e8cb28b1784fc1d51f0bb8fc9c14ef30f10
Katya Weicht <katya.weicht@gmail.com>
2025-06-13 23:15:06 -0600

    Create routes in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Shop from "./pages/Shop"
+import BookList from "./pages/BookList"
+import SearchBooks from "./pages/SearchBooks"
+import BookCard from "./pages/BookCard"
+import AdminPortal from "./pages/AdminPortal"
+import AdminForm from "./pages/AdminForm"
 
 function App() {
-  
 
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Shop />} >
+          <Route index element={<BookList />} />
+          <Route path="search" element={<SearchBooks />} />
+          <Route path=":id" element={<BookCard />} />
+        </Route>
+        <Route path="/admin" element={<AdminPortal />}> 
+          <Route path="form" element={<AdminForm/>} />
+        </Route >  
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import {Link} from "react-router-dom"
 function NavBar() {
   return (
     <nav>
-      <Link>Home</Link>
-      <Link>Shop</Link>
-      <Link>Admin Portal</Link>
+      <Link to="/" >Home</Link>
+      <Link to="/books" >Shop</Link>
+      <Link to="/admin" >Admin Portal</Link>
     </nav>
   )
 }
