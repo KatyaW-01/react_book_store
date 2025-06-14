252125c96f5dc97123655dfb329a86fcde705664
Katya Weicht <katya.weicht@gmail.com>
2025-06-14 08:39:33 -0600

    Create Admin form

--- src/pages/AdminPortal.jsx
+++ src/pages/AdminPortal.jsx
@@ -7,6 +7,19 @@ function AdminPortal() {
     <>
       <NavBar />
       <Outlet />
+      <h2>Add New Book</h2>
+      <form>
+        <label for="name">Book Name:</label>
+        <input type="text" id="name"></input>
+        <label for="author">Author:</label>
+        <input type="text" id="author"></input>
+        <label for="description">Description:</label>
+        <input type="text" id="description"></input>
+        <label for="price">Price:</label>
+        <input type="text" id="price"></input>
+        <button type="submit">Submit</button>
+      </form>
+      <button>Edit Existing Books</button>
     </>
   )
 }
